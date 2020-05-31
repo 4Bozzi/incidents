b623caa621b657e89161a6ac3f2fa316554e7ad3
Andrew Bozzi <4bozzi@gmail.com>
2020-05-30 21:10:37 -0500

    test case working

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const weather = require('./weather.js');
 const express = require('express');
+let jsonData1 = require('./incidents/F01705150050.json');
+let jsonData2 = require('./incidents/F01705150090.json');
 const app = express();
 
 const port = 3001;
@@ -7,6 +9,10 @@ const port = 3001;
 app.get('/', (req, res) => {
   weather('2019-05-02', '01:00');
   res.send('Hello World!');
+});
+
+app.get('/start', (req, res) => {
+  res.send([jsonData1, jsonData2]);
 });
 
 app.listen(port, () =>
