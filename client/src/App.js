138b4eeb13a726bfc331e4236ba0849063f1f2c5
Andrew Bozzi <4bozzi@gmail.com>
2020-05-30 21:57:06 -0500

    markers are visible on map

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,25 @@ import './App.css';
 const App = (props) => {
   const [mapData, setMapData] = useState(null);
   const mapStyles = { width: '100%', height: '100%' };
+
   useEffect(() => {
     axios.get('/start').then((response) => {
-      setMapData(response);
+      setMapData(response.data);
     });
-  });
+  }, []);
+
+  const onMarkerClick = () => {};
+  const renderMarkers = (data) =>
+    data.map((item, index) => (
+      <Marker
+        title={item.address.common_place_name}
+        name={item.address.common_place_name}
+        onClick={onMarkerClick}
+        position={{ lat: item.address.latitude, lng: item.address.longitude }}
+        data={data[index]}
+      />
+    ));
+
   return (
     <div className="App">
       <h1>Test Incident</h1>
@@ -23,7 +37,9 @@ const App = (props) => {
           lat: 37.5537575,
           lng: -77.4602617,
         }}
-      ></Map>
+      >
+        {mapData && mapData.length && renderMarkers(mapData)}
+      </Map>
     </div>
   );
 };
